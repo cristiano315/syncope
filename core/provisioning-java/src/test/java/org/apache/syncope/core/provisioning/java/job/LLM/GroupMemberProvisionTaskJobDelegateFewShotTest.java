e153fed594972b030acd296d51e1f91420dd6bbe
Cristiano315 <cristiano.pera315@gmail.com>
2026-06-10 01:53:08 +0200

    added intestations for rat

--- core/provisioning-java/src/test/java/org/apache/syncope/core/provisioning/java/job/LLM/GroupMemberProvisionTaskJobDelegateFewShotTest.java
+++ core/provisioning-java/src/test/java/org/apache/syncope/core/provisioning/java/job/LLM/GroupMemberProvisionTaskJobDelegateFewShotTest.java
@@ -1,4 +1,3 @@
-//###Test START##
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -17,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+//###Test START##
 package org.apache.syncope.core.provisioning.java.job;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
